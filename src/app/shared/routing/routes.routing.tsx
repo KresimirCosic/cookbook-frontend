ddec99c61e66cbb10de55818fd9a0583ea6dc31a
Kresimir Cosic <kresimircosic@pop-os.localdomain>
2019-12-06 14:54:56 +0100

    Redirection template
    For registration and login routes - it still needs work using localStorage which I will get to later, so I commented it out only until I get around to it.

--- src/app/shared/routing/routes.routing.tsx
+++ src/app/shared/routing/routes.routing.tsx
@@ -1,6 +1,7 @@
-import { Route, RouterStore } from "mobx-state-router";
+import { Route, RouterStore, RouterState } from "mobx-state-router";
 
 import { LOADER_ENTRY_DURATION } from "../../features/components/loader.component";
+import RootStore from "../stores/root.store";
 
 const routes: Route[] = [
   {
@@ -9,7 +10,15 @@ const routes: Route[] = [
   },
   {
     name: "register",
-    pattern: "/register"
+    pattern: "/register",
+    beforeEnter: async (fromState, toState, routerStore) => {
+      // TODO re-direct
+      // const { rootStore }: { rootStore: RootStore } = routerStore;
+      // const { authenticationStore } = rootStore;
+      // if (authenticationStore.loggedInStatus) {
+      //   return Promise.reject(new RouterState("home"));
+      // }
+    }
   },
   {
     name: "login",
@@ -17,14 +26,22 @@ const routes: Route[] = [
   },
   {
     name: "home",
-    pattern: "/"
+    pattern: "/",
+    beforeEnter: async (fromState, toState, routerStore) => {
+      // TODO re-direct
+      // const { rootStore }: { rootStore: RootStore } = routerStore;
+      // const { authenticationStore } = rootStore;
+      // if (!authenticationStore.loggedInStatus) {
+      //   return Promise.reject(new RouterState("login"));
+      // }
+    }
   }
 ];
 
 routes.forEach(route => {
   // Turning off loader upon entering
   route.onEnter = async (fromState, toState, routerStore: RouterStore) => {
-    const { rootStore } = routerStore;
+    const { rootStore }: { rootStore: RootStore } = routerStore;
     const { userInterfaceStore } = rootStore;
 
     // "Resetting" the view
@@ -37,7 +54,7 @@ routes.forEach(route => {
 
   // Turning on loader upon exiting
   route.onExit = async (fromState, toState, routerStore: RouterStore) => {
-    const { rootStore } = routerStore;
+    const { rootStore }: { rootStore: RootStore } = routerStore;
     const { userInterfaceStore } = rootStore;
 
     userInterfaceStore.turnOnLoader();
